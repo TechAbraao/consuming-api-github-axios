6e283c6b88a14d3de02a107d361835fc65eed5e1
Abraão Santos <abraao.santos@sec4you.com.br>
2025-03-01 10:54:47 -0300

    arrumando

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
-import express from "express";
-import axios, { AxiosResponse } from "axios";
-import dotenv from 'dotenv'
+import express, { Request, Response } from 'express';
+import path from "path";
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import bodyParser from "body-parser";
 
 dotenv.config();
-const app =  express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-async function searchGithub(userName: string): Promise<void> {
-    const _userName: string = userName
-    const URL: string = `https://api.github.com/users/${_userName}/repos`;
-    axios
-    .get(`${URL}`)
-    .then((res: AxiosResponse) => {
-        console.log(res.data)
-    })
-    .catch((err) => {
-        console.log(`Erro na resposta da requisição: ${err}`);
-    })
-    .finally(() => {
-            console.log("Requisição finalizada")
-    })
+app.post("/process", (req: Request, res: Response): void => {
+    const username: string = req.body.username;
+    console.log(username);
+
+    res.send(`<h1>${username}</h1>`);
+})
+
+function htmlInformationsProfile(req: Request, res: Response): void {
+    const filePath: string = path.resolve(__dirname, "html/user_informations.html");
+    res.status(200).sendFile(filePath);
 }
-
-searchGithub("TechAbraao")
+app.get("/", htmlInformationsProfile);
 
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.listen(PORT, (): void => {
     console.log("Servidor iniciou na porta: " + PORT);
-})
+});
